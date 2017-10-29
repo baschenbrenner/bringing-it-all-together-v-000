f0f933f361506b09eed77fbb0f6cd496aa1c1a21
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-29 20:37:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,7 +10,7 @@ attr_reader :id
 
     def self.create_table
       sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS dogs VALUES (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
+        CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
         SQL
 
         DB[:conn].execute(sql)
